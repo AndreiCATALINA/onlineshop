f4db4393754e7df87f31ed09f7c65ea2246b1a6b
Andrei <catalinaandrei135@gmail.com>
2024-01-14 14:42:28 +0200

    Dashboard,add edit item, list items

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {AddEditItemComponent} from "./add-edit-item/add-edit-item.component";
+import {ListItemsComponent} from "../list-items/list-items.component";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    AddEditItemComponent,
+    ListItemsComponent
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
